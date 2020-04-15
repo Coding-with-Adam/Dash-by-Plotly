ba42371125e4adb3aeea538237f18eb3ff7177e5
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-04-15 16:25:56 -0400

    Update medium_solution_challenge.py

--- Dash Components/Checklist/medium_solution_challenge.py
+++ Dash Components/Checklist/medium_solution_challenge.py
@@ -1,10 +1,12 @@
-        html.Div([
-            dcc.Checklist(
-                id='my_checklist',                    
-                options=[
-                         {'label': x, 'value': x, 'disabled':True}
-                         if x=='March' else {'label': x, 'value': x, 'disabled':False}
-                         for x in df['Month Call Made'].unique()
-                ]
-            )
-        ])
+# solution to disable only the March checkbox
+
+html.Div([
+    dcc.Checklist(
+        id='my_checklist',                    
+        options=[
+                 {'label': x, 'value': x, 'disabled':True}
+                 if x=='March' else {'label': x, 'value': x, 'disabled':False}
+                 for x in df['Month Call Made'].unique()
+        ]
+    )
+])
