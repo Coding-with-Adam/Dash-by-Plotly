5da2dc68a46926a58d6fbfa2c3ef5720d487be4e
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-04-19 01:52:04 -0400

    Update and rename easy_challenge_solution.py to easy_challenge_solution.css

---
+++ Dash Components/Checklist/easy_challenge_solution.css
@@ -0,0 +1,16 @@
+# Add opacity to line 15 inside your checklist.css form
+
+.my_box_container {
+  display: flex;
+}
+
+.my_box_input {
+  cursor: pointer;
+}
+
+.my_box_label {
+  background: #A5D6A7;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  opacity: 0.4;
+}
