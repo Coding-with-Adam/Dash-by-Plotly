886aa919ccd51cce48a74a19591b2455d83b6a72
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-04-25 01:55:55 -0400

    Create suicide_rates.py

---
+++ Other/Suicidal_Data/suicide_rates.py
@@ -0,0 +1,70 @@
+# Environment used: dash1_8_0_env
+import pandas as pd     #(version 1.0.0)
+import plotly           #(version 4.5.0)
+import plotly.io as pio
+import plotly.graph_objects as go
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Data from http://ghdx.healthdata.org/gbd-results-tool
+df = pd.read_csv("suicide-rate-1990-2017.csv")
+
+dframes=['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen',
+        'fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty','twentyone','twentytwo',
+        'twentythree','twentyfour','twentyfive','twentysix','twentyseven','twentyeight']
+
+years=[1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,
+       2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+
+n_frame={}
+# print(df[:5])
+for x, y in zip(years, dframes):
+    values=df[(df['year']==x)&(df['region']=='Europe')].nlargest(5, columns=['suicide rate (deaths per 100,000)']\
+           ).sort_values(by=['year','suicide rate (deaths per 100,000)'])
+    n_frame[y]=values
+
+# print (n_frame)
+
+#-------------------------------------------
+fig = go.Figure(
+    data=[go.Bar(x=n_frame['one']['suicide rate (deaths per 100,000)'], y=n_frame['one']['country'],orientation='h',
+                 text=n_frame['one']['suicide rate (deaths per 100,000)'], texttemplate='%{text:.3s}',
+                 textfont={'size':18}, textposition='inside', insidetextanchor='middle',
+                 width=0.9, marker={'color':n_frame['one']['color_code']})
+    ],
+    layout=go.Layout(
+        xaxis=dict(range=[0, 60], autorange=False, title=dict(text='suicide rate (deaths per 100,000)',font=dict(size=18))),
+        yaxis=dict(range=[-0.5, 5.5], autorange=False,tickfont=dict(size=14)),
+        title=dict(text='Suicide Rates per Country: 1990',font=dict(size=28),x=0.5,xanchor='center'),
+        # Add button
+        updatemenus=[dict(
+            type="buttons",
+            buttons=[dict(label="Play",
+                          method="animate",
+                          # https://github.com/plotly/plotly.js/blob/master/src/plots/animation_attributes.js
+                          args=[None,
+                          {"frame": {"duration": 1000, "redraw": True},
+                          "transition": {"duration":250,
+                          "easing": "linear"}}]
+            )]
+        )]
+    ),
+    frames=[
+            go.Frame(data=[
+                        go.Bar(x=value['suicide rate (deaths per 100,000)'], y=value['country'],
+                        orientation='h',text=value['suicide rate (deaths per 100,000)'],
+                        marker={'color':value['color_code']})
+                    ],
+                    layout=go.Layout(
+                                xaxis=dict(range=[0, 60], autorange=False),
+                                yaxis=dict(range=[-0.5, 5.5], autorange=False,tickfont=dict(size=14)),
+                                title=dict(text='Suicide Rates per Country: '+str(value['year'].values[0]),
+                                font=dict(size=28))
+                    )
+            )
+        for key, value in n_frame.items()
+    ]
+)
+
+pio.show(fig)
