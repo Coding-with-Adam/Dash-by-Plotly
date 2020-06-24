144ced76d2b73363be496fd311ceebec33cac926
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-06-23 23:54:27 -0400

    Add files via upload

---
+++ Dash_More_Advanced_Shit/CRUD_app/crud_dash_postgresql.py
@@ -0,0 +1,189 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+# app requires "pip install psycopg2" as well
+
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server, suppress_callback_exceptions=True)
+app.server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# for your home PostgreSQL test table
+# app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:adam20854@localhost/test"
+
+# for your live Heroku PostgreSQL database
+app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgres://rmyfzrwuezbehs:ebc3b6cee14731240a30c076dfc7f07\
+9ed291ec9378483ec842f7c68f8b8d368@ec2-54-211-210-149.compute-1.amazonaws.com:5432/dghfpfpcme7sp"
+
+db = SQLAlchemy(app.server)
+
+
+class Product(db.Model):
+    __tablename__ = 'productlist'
+
+    Phone = db.Column(db.String(40), nullable=False, primary_key=True)
+    Version = db.Column(db.String(40), nullable=False)
+    Price = db.Column(db.Integer, nullable=False)
+    Sales = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, phone, version, price, sales):
+        self.Phone = phone
+        self.Version = version
+        self.Price = price
+        self.Sales = sales
+
+
+# ------------------------------------------------------------------------------------------------
+
+app.layout = html.Div([
+    html.Div([
+        dcc.Input(
+            id='adding-rows-name',
+            placeholder='Enter a column name...',
+            value='',
+            style={'padding': 10}
+        ),
+        html.Button('Add Column', id='adding-columns-button', n_clicks=0)
+    ], style={'height': 50}),
+
+    dcc.Interval(id='interval_pg', interval=86400000*7, n_intervals=0),  # activated once/week or when page refreshed
+    html.Div(id='postgres_datatable'),
+
+    html.Button('Add Row', id='editing-rows-button', n_clicks=0),
+    html.Button('Save to PostgreSQL', id='save_to_postgres', n_clicks=0),
+
+    # Create notification when saving to excel
+    html.Div(id='placeholder', children=[]),
+    dcc.Store(id="store", data=0),
+    dcc.Interval(id='interval', interval=1000),
+
+    dcc.Graph(id='my_graph')
+
+])
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+@app.callback(Output('postgres_datatable', 'children'),
+              [Input('interval_pg', 'n_intervals')])
+def populate_datatable(n_intervals):
+    df = pd.read_sql_table('productlist', con=db.engine)
+    return [
+        dash_table.DataTable(
+            id='our-table',
+            columns=[{
+                         'name': str(x),
+                         'id': str(x),
+                         'deletable': False,
+                     } if x == 'Sales' or x == 'Phone'
+                     else {
+                'name': str(x),
+                'id': str(x),
+                'deletable': True,
+            }
+                     for x in df.columns],
+            data=df.to_dict('records'),
+            editable=True,
+            row_deletable=True,
+            filter_action="native",
+            sort_action="native",  # give user capability to sort columns
+            sort_mode="single",  # sort across 'multi' or 'single' columns
+            page_action='none',  # render all of the data at once. No paging.
+            style_table={'height': '300px', 'overflowY': 'auto'},
+            style_cell={'textAlign': 'left', 'minWidth': '100px', 'width': '100px', 'maxWidth': '100px'},
+            style_cell_conditional=[
+                {
+                    'if': {'column_id': c},
+                    'textAlign': 'right'
+                } for c in ['Price', 'Sales']
+            ]
+
+        ),
+    ]
+
+
+@app.callback(
+    Output('our-table', 'columns'),
+    [Input('adding-columns-button', 'n_clicks')],
+    [State('adding-rows-name', 'value'),
+     State('our-table', 'columns')],
+    prevent_initial_call=True)
+def add_columns(n_clicks, value, existing_columns):
+    if n_clicks > 0:
+        existing_columns.append({
+            'name': value, 'id': value,
+            'renamable': True, 'deletable': True
+        })
+    return existing_columns
+
+
+@app.callback(
+    Output('our-table', 'data'),
+    [Input('editing-rows-button', 'n_clicks')],
+    [State('our-table', 'data'),
+     State('our-table', 'columns')],
+    prevent_initial_call=True)
+def add_row(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c['id']: '' for c in columns})
+    return rows
+
+
+@app.callback(
+    Output('my_graph', 'figure'),
+    [Input('our-table', 'data')],
+    prevent_initial_call=True)
+def display_graph(data):
+    # df_fig = pd.DataFrame(data)
+    # fig = px.bar(df_fig, x='Phone', y='Sales')
+
+    pg_filtered = db.session.query(Product.Phone, Product.Sales)
+    phone_c = [x.Phone for x in pg_filtered]
+    sales_c = [x.Sales for x in pg_filtered]
+    fig = go.Figure([go.Bar(x=phone_c, y=sales_c)])
+
+    return fig
+
+
+@app.callback(
+    [Output('placeholder', 'children'),
+     Output("store", "data")],
+    [Input('save_to_postgres', 'n_clicks'),
+     Input("interval", "n_intervals")],
+    [State('our-table', 'data'),
+     State('store', 'data')],
+    prevent_initial_call=True)
+def df_to_csv(n_clicks, n_intervals, dataset, s):
+    output = html.Plaintext("The data has been saved to your PostgreSQL database.",
+                            style={'color': 'green', 'font-weight': 'bold', 'font-size': 'large'})
+    no_output = html.Plaintext("", style={'margin': "0px"})
+
+    input_triggered = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
+
+    if input_triggered == "save_to_postgres":
+        s = 6
+        pg = pd.DataFrame(dataset)
+        pg.to_sql("productlist", con=db.engine, if_exists='replace', index=False)
+        return output, s
+    elif input_triggered == 'interval' and s > 0:
+        s = s - 1
+        if s > 0:
+            return output, s
+        else:
+            return no_output, s
+    elif s == 0:
+        return no_output, s
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
