72458f32cb167492fea2b89b0eb76ae6ffa0ee76
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-08-07 16:35:24 -0400

    Update bootstrap_modal.py

--- Bootstrap/bootstrap_modal.py
+++ Bootstrap/bootstrap_modal.py
@@ -8,10 +8,7 @@ import pandas as pd
 
 df = pd.read_csv("Berlin_crimes.csv")
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-alert = dbc.Alert("Please choose Districts from dropdown to avoid further disappointment!", color="danger",
-                  dismissable=True),  # use dismissable or duration=5000 for alert to close in x milliseconds
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP]) # https://bootswatch.com/default/
 
 modal = html.Div(
     [
@@ -59,10 +56,13 @@ modal = html.Div(
             backdrop=True,    # True, False or Static for modal to not be closed by clicking on backdrop
             scrollable=True,  # False or True if modal has a lot of text
             centered=True,    # True, False
-            fade=True  # True, False
+            fade=True         # True, False
         ),
     ]
 )
+
+alert = dbc.Alert("Please choose Districts from dropdown to avoid further disappointment!", color="danger",
+                  dismissable=True),  # use dismissable or duration=5000 for alert to close in x milliseconds
 
 image_card = dbc.Card(
     [
@@ -88,7 +88,7 @@ graph_card = dbc.Card(
             [
                 html.H4("Graffiti in Berlin 2012-2019", className="card-title", style={"text-align": "center"}),
                 dbc.Button(
-                    "About Berlin", id="popover-target", color="info"
+                    "About Berlin", id="popover-bottom-target", color="info"
                 ),
                 dbc.Popover(
                     [
@@ -97,8 +97,9 @@ graph_card = dbc.Card(
                             "Berlin (/bɜːrˈlɪn/; German: [bɛʁˈliːn] is the capital and largest city of Germany by both area and population. Its 3,769,495 (2019) inhabitants make it the most populous city proper of the European Union. The city is one of Germany's 16 federal states. It is surrounded by the state of Brandenburg, and contiguous with Potsdam, Brandenburg's capital. The two cities are at the center of the Berlin-Brandenburg capital region, which is, with about six million inhabitants and an area of more than 30,000 km2, Germany's third-largest metropolitan region after the Rhine-Ruhr and Rhine-Main regions. (Wikipedia)"),
                     ],
                     id="popover",
+                    target="popover-bottom-target",  # needs to be the same as dbc.Button id
+                    placement="bottom",
                     is_open=False,
-                    target="popover-target",  # attach the dbc.popover to the button
                 ),
                 dcc.Graph(id='line_chart', figure={}),
 
@@ -118,7 +119,7 @@ app.layout = html.Div([
 
 @app.callback(
     Output("popover", "is_open"),
-    [Input("popover-target", "n_clicks")],
+    [Input("popover-bottom-target", "n_clicks")],
     [State("popover", "is_open")],
 )
 def toggle_popover(n, is_open):
