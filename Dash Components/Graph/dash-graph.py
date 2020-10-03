57439e327656d340efad1003cd1f60574044ca43
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-10-03 11:49:04 -0400

    Update dash-graph.py

--- Dash Components/Graph/dash-graph.py
+++ Dash Components/Graph/dash-graph.py
@@ -6,7 +6,8 @@ import plotly.express as px
 
 df = px.data.gapminder()
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     dcc.Dropdown(id='dpdn2', value=['Germany','Brazil'], multi=True,
