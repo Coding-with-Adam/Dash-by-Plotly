bb74dc0b8f4cbe246f319a92e2d0440ad09d3e5a
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-10-31 00:29:21 -0400

    Update excel_plotly_dash.py

--- Other/Excel_Dash/excel_plotly_dash.py
+++ Other/Excel_Dash/excel_plotly_dash.py
@@ -2,21 +2,19 @@ import dash
 import plotly.express as px
 import pandas as pd
 
+# Data Exploration with Pandas (python)
+# -----------------------------------------------------------------
 
-# Data Exploration with Pandas (Python)
-# ----------------------------------------------------------------
+df = pd.read_csv("vgsales.csv")
 
-df = pd.read_csv("vgsales.csv")  # GregorySmith Kaggle
-
-print(df[:3])
-print(df.iloc[:5, [2,3,4,5,10]])
-print(df['Genre'].nunique())
-print(df['Genre'].unique())
-print(sorted(df['Year'].unique()))
-
+print(df[:5])
+print(df.iloc[:5, [2,3,5,10]])
+print(df.Genre.nunique())
+print(df.Genre.unique())
+print(sorted(df.Year.unique()))
 
 # Data Visualization with Plotly (Python)
-# ----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 fig_pie = px.pie(data_frame=df, names='Genre', values='Japan Sales')
 fig_pie = px.pie(data_frame=df, names='Genre', values='North American Sales')
@@ -28,14 +26,14 @@ fig_hist.show()
 
 df = df.groupby(['Year','Genre']).sum()
 df = df.reset_index()
-print(df[-6:])
+print(df[-5:])
+
 fig_bar = px.bar(data_frame=df, x='Year', y='Japan Sales', facet_col='Genre',
                  facet_col_wrap=3)
 fig_bar.show()
 
-
-# Interactive Graphs with Dash (Python)
-# ----------------------------------------------------------------
+# Interactive Graphs with Dash (Python, R, Julia)
+# -----------------------------------------------------------------
 
 import dash_core_components as dcc
 import dash_html_components as html
@@ -43,26 +41,25 @@ from dash.dependencies import Output, Input
 
 app = dash.Dash(__name__)
 
-app.layout= html.Div([
-    html.H1("Excel Analysis of Graphs"),
+app.layout=html.Div([
+    html.H1("Graph Analysis with Charming Data"),
     dcc.Dropdown(id='genre-choice',
-                 options=[{'label': x, 'value': x}
-                          for x in sorted(df['Genre'].unique())],
+                 options=[{'label':x, 'value':x}
+                          for x in sorted(df.Genre.unique())],
                  value='Action'
                  ),
-    dcc.Graph(id='my-graph', figure={}),
+    dcc.Graph(id='my-graph',
+              figure={}),
 ])
-
-
 @app.callback(
     Output(component_id='my-graph', component_property='figure'),
     Input(component_id='genre-choice', component_property='value')
 )
-def interactive_graphing(value):
+def interactive_graphs(value):
     print(value)
     dff = df[df.Genre==value]
-    fig_hist = px.bar(data_frame=dff, x='Year', y='Japan Sales')
-    return fig_hist
+    fig = px.histogram(data_frame=dff, x='Year', y='Japan Sales')
+    return fig
 
 
 if __name__=='__main__':
