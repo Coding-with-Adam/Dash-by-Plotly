1ab8ee6822034afcc987354948cd9088270c56b0
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-11-09 00:10:48 -0500

    Update chained_fig_postset.py

--- Callbacks/chained_callback/chained_fig_postset.py
+++ Callbacks/chained_callback/chained_fig_postset.py
@@ -10,7 +10,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# Data from U.S. Congress, Joint Economic Committee, Social Capital Project.
+# Data from U.S. Congress, Joint Economic Committee, Social Capital Project. https://www.jec.senate.gov/public/index.cfm/republicans/2018/4/the-geography-of-social-capital-in-america
 df = pd.read_csv("social-capital-project.csv")
 
 app.layout = html.Div([
