763a7c5ff0f2df90353ed8dd70875d212c3e441a
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-11-13 21:40:54 -0500

    Delete app.py

--- Dash_More_Advanced_Shit/Multipage App/app.py
+++
@@ -1,8 +0,0 @@
-import dash
-
-# meta_tags are required for the app layout to be mobile responsive
-app = dash.Dash(__name__, suppress_callback_exceptions=True,
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
-                )
-server = app.server
