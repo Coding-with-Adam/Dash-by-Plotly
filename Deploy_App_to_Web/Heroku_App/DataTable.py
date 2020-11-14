f6d15d465c992d72d016bb8bd31416bb44e0b4ff
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-11-13 21:32:49 -0500

    Create DataTable.py

---
+++ Deploy_App_to_Web/Heroku_App/DataTable.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import plotly.express as px
+
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+server = app.server
+
+#---------------------------------------------------------------
+#Taken from https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases
+df = pd.read_csv("COVID-19-geographic-disbtribution-worldwide-2020-03-29.csv")
+
+dff = df.groupby('countriesAndTerritories', as_index=False)[['deaths','cases']].sum()
+print (dff[:5])
+#---------------------------------------------------------------
+app.layout = html.Div([
+    html.Div([
+        dash_table.DataTable(
+            id='datatable_id',
+            data=dff.to_dict('records'),
+            columns=[
+                {"name": i, "id": i, "deletable": False, "selectable": False} for i in dff.columns
+            ],
+            editable=False,
+            filter_action="native",
+            sort_action="native",
+            sort_mode="multi",
+            row_selectable="multi",
+            row_deletable=False,
+            selected_rows=[],
+            page_action="native",
+            page_current= 0,
+            page_size= 6,
+            # page_action='none',
+            # style_cell={
+            # 'whiteSpace': 'normal'
+            # },
+            # fixed_rows={ 'headers': True, 'data': 0 },
+            # virtualization=False,
+            style_cell_conditional=[
+                {'if': {'column_id': 'countriesAndTerritories'},
+                 'width': '40%', 'textAlign': 'left'},
+                {'if': {'column_id': 'deaths'},
+                 'width': '30%', 'textAlign': 'left'},
+                {'if': {'column_id': 'cases'},
+                 'width': '30%', 'textAlign': 'left'},
+            ],
+        ),
+    ],className='row'),
+
+    html.Div([
+        html.Div([
+            dcc.Dropdown(id='linedropdown',
+                options=[
+                         {'label': 'Deaths', 'value': 'deaths'},
+                         {'label': 'Cases', 'value': 'cases'}
+                ],
+                value='deaths',
+                multi=False,
+                clearable=False
+            ),
+        ],className='six columns'),
+
+        html.Div([
+        dcc.Dropdown(id='piedropdown',
+            options=[
+                     {'label': 'Deaths', 'value': 'deaths'},
+                     {'label': 'Cases', 'value': 'cases'}
+            ],
+            value='cases',
+            multi=False,
+            clearable=False
+        ),
+        ],className='six columns'),
+
+    ],className='row'),
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='linechart'),
+        ],className='six columns'),
+
+        html.Div([
+            dcc.Graph(id='piechart'),
+        ],className='six columns'),
+
+    ],className='row'),
+
+
+])
+
+#------------------------------------------------------------------
+@app.callback(
+    [Output('piechart', 'figure'),
+     Output('linechart', 'figure')],
+    [Input('datatable_id', 'selected_rows'),
+     Input('piedropdown', 'value'),
+     Input('linedropdown', 'value')]
+)
+def update_data(chosen_rows,piedropval,linedropval):
+    if len(chosen_rows)==0:
+        df_filterd = dff[dff['countriesAndTerritories'].isin(['China','Iran','Spain','Italy'])]
+    else:
+        print(chosen_rows)
+        df_filterd = dff[dff.index.isin(chosen_rows)]
+
+    pie_chart=px.pie(
+            data_frame=df_filterd,
+            names='countriesAndTerritories',
+            values=piedropval,
+            hole=.3,
+            labels={'countriesAndTerritories':'Countries'}
+            )
+
+
+    #extract list of chosen countries
+    list_chosen_countries=df_filterd['countriesAndTerritories'].tolist()
+    #filter original df according to chosen countries
+    #because original df has all the complete dates
+    df_line = df[df['countriesAndTerritories'].isin(list_chosen_countries)]
+
+    line_chart = px.line(
+            data_frame=df_line,
+            x='dateRep',
+            y=linedropval,
+            color='countriesAndTerritories',
+            labels={'countriesAndTerritories':'Countries', 'dateRep':'date'},
+            )
+    line_chart.update_layout(uirevision='foo')
+
+    return (pie_chart,line_chart)
+
+#------------------------------------------------------------------
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
