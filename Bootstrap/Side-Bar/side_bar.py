4cbb1f4b052ffc06752cdd8c7404498c6ffb6430
Coding-with-Adam <32049495+Coding-with-Adam@users.noreply.github.com>
2020-12-25 17:23:14 -0500

    Update side_bar.py

--- Bootstrap/Side-Bar/side_bar.py
+++ Bootstrap/Side-Bar/side_bar.py
@@ -1,3 +1,4 @@
+# Code source: https://dash-bootstrap-components.opensource.faculty.ai/examples/simple-sidebar/
 import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
