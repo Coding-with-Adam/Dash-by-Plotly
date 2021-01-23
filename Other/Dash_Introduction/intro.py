a4cd896c018433dfa159db25824a1575092f675a
Coding-with-Adam <info@charmingdata.org>
2021-01-23 13:47:32 -0500

    Update intro.py

--- Other/Dash_Introduction/intro.py
+++ Other/Dash_Introduction/intro.py
@@ -9,9 +9,9 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-# ------------------------------------------------------------------------------
-# Import and clean data (importing csv into pandas)
-df = pd.read_csv("intro_bees.csv")
+# ---------- Import and clean data (importing csv into pandas)
+# df = pd.read_csv("intro_bees.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Other/Dash_Introduction/intro_bees.csv")
 
 df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
 df.reset_index(inplace=True)
