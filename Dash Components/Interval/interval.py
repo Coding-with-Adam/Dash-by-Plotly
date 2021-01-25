a4cd8c2ba8cbc5a601a714f5c1689dc36289c518
Coding-with-Adam <info@charmingdata.org>
2021-01-25 09:06:24 -0500

    Update interval.py

--- Dash Components/Interval/interval.py
+++ Dash Components/Interval/interval.py
@@ -1,7 +1,7 @@
-import plotly           #(version 4.5.4) pip install plotly==4.5.4
+import plotly          
 import plotly.graph_objects as go
 
-import dash             #(version 1.9.1) pip install dash==1.9.1
+import dash             
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
