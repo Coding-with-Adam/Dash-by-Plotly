03e52454e9a44eb9043a8d2b9c1648c9261c0879
Charming Data <info@charmingdata.org>
2021-03-10 13:45:25 -0500

    Add files via upload

--- Other/DPhi Presentation/sunburst.py
+++ Other/DPhi Presentation/sunburst.py
@@ -7,18 +7,19 @@ df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plo
 
 df = df[df["State"].isin(['NY', 'CA', 'TX'])]
 df = df[df["Victim's race"].isin(["White", "Black", "Hispanic", "Asian"])]
-df["Victim's age"] = pd.to_numeric(df["Victim's age"], errors='coerce').fillna(0)
+df["Victim's age"] = pd.to_numeric(df["Victim's age"], errors='coerce')
 
 # build the graph---------------------------------------------------------
 fig = px.sunburst(
     data_frame=df,
     path=["Weapon", 'State', "Victim's race"],  # Root, branches, leaves
     color="Weapon",
-    # color="Victim's age",
-    # title="7-year Breakdown of Deaths by Police",
-    # template='ggplot2',               # 'ggplot2', 'seaborn', 'simple_white', 'plotly',
-    #                                   # 'plotly_white', 'plotly_dark', 'presentation',
-    #                                   # 'xgridoff', 'ygridoff', 'gridon', 'none'
+    #     color="Victim's age",
+    #     title="7-year Breakdown of Deaths by Police",
+    #     template='plotly_dark',           # 'ggplot2', 'seaborn', 'simple_white', 'plotly',
+    #                                       # 'plotly_white', 'plotly_dark', 'presentation',
+    #                                       # 'xgridoff', 'ygridoff', 'gridon', 'none'
+
 )
 
 fig.show()
