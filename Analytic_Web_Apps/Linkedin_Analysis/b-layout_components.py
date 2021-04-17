898f1656bee5c09028730229d564e4af5473e131
Charming Data <info@charmingdata.org>
2021-04-17 17:51:52 -0400

    Add files via upload

---
+++ Analytic_Web_Apps/Linkedin_Analysis/b-layout_components.py
@@ -0,0 +1,147 @@
+import dash                              # pip install dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Output, Input
+
+from dash_extensions import Lottie       # pip install dash-extensions
+import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
+import plotly.express as px              # pip install plotly
+import pandas as pd                      # pip install pandas
+from datetime import date
+import calendar
+from wordcloud import WordCloud          # pip install wordcloud
+
+
+# Lottie by Emil - https://github.com/thedirtyfew/dash-extensions
+url_coonections = "https://assets9.lottiefiles.com/private_files/lf30_5ttqPi.json"
+url_companies = "https://assets9.lottiefiles.com/packages/lf20_EzPrWM.json"
+url_msg_in = "https://assets9.lottiefiles.com/packages/lf20_8wREpI.json"
+url_msg_out = "https://assets2.lottiefiles.com/packages/lf20_Cc8Bpg.json"
+url_reactions = "https://assets2.lottiefiles.com/packages/lf20_nKwET0.json"
+options = dict(loop=True, autoplay=True, rendererSettings=dict(preserveAspectRatio='xMidYMid slice'))
+
+
+# Bootstrap themes by Ann: https://hellodash.pythonanywhere.com/theme_explorer
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardImg(src='/assets/linkedin-logo2.png') # 150px by 45px
+            ],className='mb-2'),
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.CardLink("Charming Data", target="_blank",
+                                 href="https://youtube.com/charmingdata"
+                    )
+                ])
+            ]),
+        ], width=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.DatePickerSingle(
+                        id='my-date-picker-start',
+                        date=date(2018, 1, 1),
+                        className='ml-5'
+                    ),
+                    dcc.DatePickerSingle(
+                        id='my-date-picker-end',
+                        date=date(2021, 4, 4),
+                        className='mb-2 ml-2'
+                    ),
+                ])
+            ], color="info"),
+        ], width=8),
+    ],className='mb-2 mt-2'),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader(Lottie(options=options, width="67%", height="67%", url=url_coonections)),
+                dbc.CardBody([
+                    html.H6('Connections'),
+                    html.H2(id='content-connections', children="000")
+                ], style={'textAlign':'center'})
+            ]),
+        ], width=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader(Lottie(options=options, width="32%", height="32%", url=url_companies)),
+                dbc.CardBody([
+                    html.H6('Companies'),
+                    html.H2(id='content-companies', children="000")
+                ], style={'textAlign':'center'})
+            ]),
+        ], width=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader(Lottie(options=options, width="25%", height="25%", url=url_msg_in)),
+                dbc.CardBody([
+                    html.H6('Invites received'),
+                    html.H2(id='content-msg-in', children="000")
+                ], style={'textAlign':'center'})
+            ]),
+        ], width=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader(Lottie(options=options, width="53%", height="53%", url=url_msg_out)),
+                dbc.CardBody([
+                    html.H6('Invites sent'),
+                    html.H2(id='content-msg-out', children="000")
+                ], style={'textAlign': 'center'})
+            ]),
+        ], width=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader(Lottie(options=options, width="25%", height="25%", url=url_reactions)),
+                dbc.CardBody([
+                    html.H6('Reactions'),
+                    html.H2(id='content-reactions', children="000")
+                ], style={'textAlign': 'center'})
+            ]),
+        ], width=2),
+    ],className='mb-2'),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='line-chart', figure={}),
+                ])
+            ]),
+        ], width=6),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='bar-chart', figure={}),
+                ])
+            ]),
+        ], width=4),
+    ],className='mb-2'),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='TBD', figure={}),
+                ])
+            ]),
+        ], width=3),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='pie-chart', figure={}),
+                ])
+            ]),
+        ], width=3),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='wordcloud', figure={}),
+                ])
+            ]),
+        ], width=4),
+    ],className='mb-2'),
+], fluid=True)
+
+if __name__=='__main__':
+    app.run_server(debug=False, port=8002)
