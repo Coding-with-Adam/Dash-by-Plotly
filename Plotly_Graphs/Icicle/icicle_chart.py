3b5c7f1092ba8c400244c7963ec1b0f0436d12f9
Charming Data <info@charmingdata.org>
2021-07-10 15:00:38 -0400

    Add files via upload

---
+++ Plotly_Graphs/Icicle/icicle_chart.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("Bills_of_Sale_of_Enslaved_Individuals__1718-1862.csv")
+print(df.head()[['Period', 'Locality']])
+
+fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality'],
+                values='Slaves')
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality'],
+#                 values='Slaves', color='Locality')
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality', 'Gender'],
+#                 values='Slaves', color='Gender')
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality', 'Enslaver_Buyer'],
+#                 values='Slaves')
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality', 'Enslaver_Buyer'],
+#                 values='Slaves', color='Age')
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality', 'Enslaver_Buyer'],
+#                 values='Slaves', color='Age', color_continuous_scale='RdBu')
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality'],
+#                 values='Slaves', color='Locality')
+# fig.update_traces(root_color="lightgrey", tiling=dict(orientation='h', flip='x'))
+
+# fig = px.icicle(df, path=[px.Constant("all"), 'Period', 'Locality'],
+#                 values='Slaves', color='Locality')
+# fig.update_traces(root_color="lightgrey", tiling=dict(orientation='v', flip='y'))
+
+
+fig.update_traces(root_color="lightgrey", tiling=dict(orientation='h'))
+
+fig.show()
