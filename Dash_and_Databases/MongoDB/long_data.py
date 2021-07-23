9412df8e256908f91110f99e462c7683829bf30f
Charming Data <info@charmingdata.org>
2021-07-23 14:58:03 -0400

    Add files via upload

---
+++ Dash_and_Databases/MongoDB/long_data.py
@@ -0,0 +1,354 @@
+db.shelterA.insertMany([
+    {
+        "owner": "Marv",
+        "animal": "cat",
+        "breed": "tabi",
+        "age": 1,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Adam",
+        "animal": "cat",
+        "breed": "longthair",
+        "age": 3,
+        "health": "great",
+        "neutered": false
+    },
+    {
+        "owner": "Jim",
+        "animal": "cat",
+        "breed": "shorthair",
+        "age": 2,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Sebastian",
+        "animal": "dog",
+        "breed": "french",
+        "age": 5,
+        "health": "bad",
+        "neutered": true
+    },
+    {
+        "owner": "Sindy",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 6,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Marvin",
+        "animal": "cat",
+        "breed": "tabi",
+        "age": 7,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Samuel",
+        "animal": "dog",
+        "breed": "buldog",
+        "age": 4,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Blake",
+        "animal": "cat",
+        "breed": "shorthair",
+        "age": 4,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Orit",
+        "animal": "dog",
+        "breed": "french",
+        "age": 3,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Emmanuel",
+        "animal": "dog",
+        "breed": "poodle",
+        "age": 7,
+        "health": "bad",
+        "neutered": false
+    },
+    {
+        "owner": "Gonzalez",
+        "animal": "dog",
+        "breed": "french",
+        "age": 2,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Izeta",
+        "animal": "cat",
+        "breed": "longthair",
+        "age": 5,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Justin",
+        "animal": "cat",
+        "breed": "shorthair",
+        "age": 7,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Daisy",
+        "animal": "dog",
+        "breed": "french",
+        "age": 2,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Mickey",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 2,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Charles",
+        "animal": "cat",
+        "breed": "tabi",
+        "age": 5,
+        "health": "bad",
+        "neutered": false
+    },
+    {
+        "owner": "Juaquin",
+        "animal": "dog",
+        "breed": "buldog",
+        "age": 7,
+        "health": "bad",
+        "neutered": false
+    },
+    {
+        "owner": "Clement",
+        "animal": "cat",
+        "breed": "longhair",
+        "age": 1,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Orleon",
+        "animal": "cat",
+        "breed": "shorthair",
+        "age": 6,
+        "health": "great",
+        "neutered": true
+    },
+    {
+        "owner": "Elijah",
+        "animal": "dog",
+        "breed": "poodle",
+        "age": 6,
+        "health": "great",
+        "neutered": false
+    },
+    {
+        "owner": "Prajat",
+        "animal": "dog",
+        "breed": "buldog",
+        "age": 3,
+        "health": "good",
+        "neutered": true
+    },
+    {
+        "owner": "Simon",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 5,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Lyla",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 4,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Sam",
+        "animal": "dog",
+        "breed": "labrador",
+        "age": 4,
+        "health": "average",
+        "neutered": false
+    },
+    {
+        "owner": "Seba",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 7,
+        "health": "bad",
+        "neutered": true
+    },
+    {
+        "owner": "Clemente",
+        "animal": "cat",
+        "breed": "shorthair",
+        "age": 16,
+        "health": "great",
+        "neutered": true
+    },
+    {
+        "owner": "Zeta",
+        "animal": "dog",
+        "breed": "poodle",
+        "age": 12,
+        "health": "great",
+        "neutered": false
+    },
+    {
+        "owner": "Prajatakan",
+        "animal": "dog",
+        "breed": "buldog",
+        "age": 13,
+        "health": "good",
+        "neutered": true
+    },
+    {
+        "owner": "Jayne",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 12,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Hermina",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 16,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Hasan",
+        "animal": "dog",
+        "breed": "labrador",
+        "age": 14,
+        "health": "average",
+        "neutered": false
+    },
+    {
+        "owner": "Sebasel",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 15,
+        "health": "bad",
+        "neutered": false
+    },
+    {
+        "owner": "Zetal",
+        "animal": "dog",
+        "breed": "poodle",
+        "age": 13,
+        "health": "great",
+        "neutered": false
+    },
+    {
+        "owner": "Praj",
+        "animal": "dog",
+        "breed": "buldog",
+        "age": 9,
+        "health": "good",
+        "neutered": true
+    },
+    {
+        "owner": "Jaynee",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 11,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Azeta",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 8,
+        "health": "good",
+        "neutered": false
+    },
+    {
+        "owner": "Hassan",
+        "animal": "dog",
+        "breed": "labrador",
+        "age": 6,
+        "health": "average",
+        "neutered": false
+    },
+    {
+        "owner": "Sadiy",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 10,
+        "health": "bad",
+        "neutered": false
+    },
+    {
+        "owner": "Zerox",
+        "animal": "dog",
+        "breed": "poodle",
+        "age": 11,
+        "health": "great",
+        "neutered": true
+    },
+    {
+        "owner": "Asim",
+        "animal": "dog",
+        "breed": "buldog",
+        "age": 8,
+        "health": "good",
+        "neutered": true
+    },
+    {
+        "owner": "Shenelle",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 12,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Spity",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 11,
+        "health": "good",
+        "neutered": true
+    },
+    {
+        "owner": "Tom",
+        "animal": "dog",
+        "breed": "labrador",
+        "age": 5,
+        "health": "average",
+        "neutered": true
+    },
+    {
+        "owner": "Numa",
+        "animal": "cat",
+        "breed": "sphynx",
+        "age": 9,
+        "health": "bad",
+        "neutered": true
+    },
+])
