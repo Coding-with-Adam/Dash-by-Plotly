095b098aa35d7319959d377abb41df78e843b64d
Charming Data <info@charmingdata.org>
2021-10-02 20:03:16 -0400

    Update selecting.py

--- DataTable/Sort-filter-select/selecting.py
+++ DataTable/Sort-filter-select/selecting.py
@@ -1,11 +1,14 @@
-# This Code was written by Ann Marie
+# This Code was written by Ann Marie - a Plotly Forum Moderator
 from dash import Dash, dcc, html, Input, Output, dash_table, no_update  # Dash version >= 2.0.0
 import plotly.express as px
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = px.data.gapminder()
 df["id"] = df.index
+# print(df.head(15))
 dff = df[df.year == 2007]
 columns = ["country", "continent", "lifeExp", "pop", "gdpPercap"]
 color = {"lifeExp": "#636EFA", "pop": "#EF553B", "gdpPercap": "#00CC96"}
@@ -26,9 +29,11 @@ app.layout = html.Div(
                 ),
             ],
             style={"margin": 50},
+            className="five columns"
         ),
-        html.Div(id="output-graph"),
-    ]
+        html.Div(id="output-graph", className="six columns"),
+    ],
+    className="row"
 )
 
 
@@ -40,14 +45,21 @@ def cell_clicked(active_cell):
         return no_update
 
     row = active_cell["row_id"]
-    col = active_cell["column_id"]
+    print(f"row id: {row}")
+
     country = df.at[row, "country"]
+    print(country)
+
+    col = active_cell["column_id"]
+    print(f"column id: {col}")
+    print("---------------------")
+
     y = col if col in ["pop", "gdpPercap"] else "lifeExp"
 
     fig = px.line(
         df[df["country"] == country], x="year", y=y, title=" ".join([country, y])
     )
-    fig.update_layout(title={"font_size": 20})
+    fig.update_layout(title={"font_size": 20},  title_x=0.5, margin=dict(t=190, r=15, l=5, b=5))
     fig.update_traces(line=dict(color=color[y]))
 
     return dcc.Graph(figure=fig)
