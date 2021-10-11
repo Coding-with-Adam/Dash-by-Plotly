7326519f837f90459acd4a06315237898c7beb2b
Dave Spencer <ds.liberty@gmail.com>
2021-10-11 03:13:45 +0000

    pip install dbc

--- DecisionNerd/complete_bootstrap-tutorial.py
+++ DecisionNerd/complete_bootstrap-tutorial.py
@@ -1,7 +1,7 @@
 # import libraries
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import dash_bootstrap_components as dbc
