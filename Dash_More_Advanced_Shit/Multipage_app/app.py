28062adf35533b29be112a64e50124afe87dbc43
Charming Data <info@charmingdata.org>
2021-12-12 00:18:27 -0500

    Update app.py

--- Dash_More_Advanced_Shit/Multipage_app/app.py
+++ Dash_More_Advanced_Shit/Multipage_app/app.py
@@ -1,7 +1,7 @@
 import dash  # pip install dash
 import dash_labs as dl  # pip install dash-labs
 import dash_bootstrap_components as dbc # pip install dash-bootstrap-components
-
+# Code from: https://github.com/plotly/dash-labs/tree/main/docs/demos/multi_page_example1
 
 app = dash.Dash(
     __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.BOOTSTRAP]
