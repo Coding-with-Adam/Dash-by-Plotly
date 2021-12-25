00898c7385996fa46f296c3a166a20a6eb4bb363
Charming Data <info@charmingdata.org>
2021-12-25 16:10:27 -0500

    Create bargraph.py

---
+++ Dash_More_Advanced_Shit/Sharing_data_multipage_app/pages/bargraph.py
@@ -0,0 +1,44 @@
+import dash
+
+dash.register_page(__name__, path="/")
+
+from dash import Dash, dcc, html, Input, Output, State, callback, dash_table
+import plotly.express as px
+import pandas as pd
+
+
+layout = html.Div(
+    [
+        html.P("Choose Day:"),
+        html.Div(id="dropdown-container", children=[]),
+        html.Div(id="bar-container", children=[]),
+    ]
+)
+
+@callback(Output("dropdown-container", "children"), Input("stored-data", "data"))
+def populate_dropdownvalues(data):
+    dff = pd.DataFrame(data)
+    return dcc.Dropdown(
+        id="dropdown",
+        options=[{"label": x, "value": x} for x in dff.day.unique()],
+        value=dff.day.unique()[0],
+        clearable=False,
+        style={"width": "50%"},
+        persistence=True,
+        persistence_type="session"
+    ),
+
+
+@callback(
+    [Output("bar-container", "children"),
+    Output("dropdown-value", "data")],
+    [Input("dropdown", "value"),
+     State("stored-data", "data")]
+)
+def graph_and_table(dropdown_day, data):
+    dff = pd.DataFrame(data)
+    dff = dff[dff["day"] == dropdown_day]
+    fig = px.bar(dff, x="sex", y="total_bill", color="smoker", barmode="group")
+    return dcc.Graph(figure=fig), dropdown_day
+
+
