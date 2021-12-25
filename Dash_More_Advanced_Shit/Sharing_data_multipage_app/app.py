71e279736b3ecad6a1c84ad739b2e41dafda1a58
Charming Data <info@charmingdata.org>
2021-12-25 16:09:57 -0500

    Create app.py

---
+++ Dash_More_Advanced_Shit/Sharing_data_multipage_app/app.py
@@ -0,0 +1,44 @@
+import dash
+from dash import dcc, callback, Output, Input, dash_table # pip install dash
+import dash_labs as dl  # pip install dash-labs
+import dash_bootstrap_components as dbc # pip install dash-bootstrap-components
+import plotly.express as px
+
+
+df = px.data.tips()
+# convert dataframe to list of dictionaries because dcc.Store
+# accepts dict | list | number | string | boolean
+df = df.to_dict('records')
+
+
+app = dash.Dash(
+    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.FLATLY], suppress_callback_exceptions=True
+)
+
+navbar = dbc.NavbarSimple(
+    dbc.DropdownMenu(
+        [
+            dbc.DropdownMenuItem(page["name"], href=page["path"])
+            for page in dash.page_registry.values()
+            if page["module"] != "pages.not_found_404"
+        ],
+        nav=True,
+        label="More Pages",
+    ),
+    brand="Multi Page App Plugin Demo",
+    color="primary",
+    dark=True,
+    className="mb-2",
+)
+
+app.layout = dbc.Container(
+    [navbar,
+     dcc.Store(id="stored-data", data=df),
+     dcc.Store(id="dropdown-value", data={}),
+     dl.plugins.page_container],
+    fluid=True,
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8003)
