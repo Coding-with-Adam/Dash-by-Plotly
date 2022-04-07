33ce473f4ec6e868523362a8b20c2401692bbe48
Charming Data <info@charmingdata.org>
2022-04-07 09:28:10 -0400

    Update mexico-dash.py

--- Other/Monterrey/mexico-dash.py
+++ Other/Monterrey/mexico-dash.py
@@ -65,6 +65,9 @@ if __name__ == '__main__':
     app.run_server(debug=True)
 
 # Resources:
-# Plotly Forum - community.plotly.com
-# YouTube channel
-# Dash Docs
+# Plotly Forum - https://www.community.plotly.com
+# YouTube channel - https://www.youtube.com/charmingdata
+# Dash Docs - https://dash.plotly.com/
+# Color scales - https://plotly.com/python/builtin-colorscales/
+# Plotly graphs - https://plotly.com/python/
+# Plotly Express parameteres - https://plotly.github.io/plotly.py-docs/plotly.express.html
