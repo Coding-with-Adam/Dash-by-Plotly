39473c526890939b4f113b532d415afec32d90e3
Adam Schroeder <adamschroeder.m@gmail.com>
2022-06-09 14:54:46 -0400

    updated line space

--- Other/Monterrey/mexico-dash.py
+++ Other/Monterrey/mexico-dash.py
@@ -63,12 +63,3 @@ def update_graph(nights_value, prices_value):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-# Resources:
-# Plotly Forum - https://www.community.plotly.com
-# YouTube channel - https://www.youtube.com/charmingdata
-# Dash Docs - https://dash.plotly.com/
-# Color scales - https://plotly.com/python/builtin-colorscales/
-# Plotly graphs - https://plotly.com/python/
-# Plotly Express parameteres - https://plotly.github.io/plotly.py-docs/plotly.express.html
-# Dash Bootstrap Components - https://dash-bootstrap-components.opensource.faculty.ai/docs/components/alert/
