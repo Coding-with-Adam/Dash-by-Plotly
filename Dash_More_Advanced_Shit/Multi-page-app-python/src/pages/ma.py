e3c7fb09631b5b0cb80220baa3b8239aa14027e8
Charming Data <info@charmingdata.org>
2022-07-17 18:09:17 -0400

    Update ma.py

--- Dash_More_Advanced_Shit/Multi-page-app-python/src/pages/ma.py
+++ Dash_More_Advanced_Shit/Multi-page-app-python/src/pages/ma.py
@@ -15,21 +15,12 @@ df = pd.read_csv(DATA_PATH.joinpath('MA-data.csv'))
 
 
 def build_graph(dff):
-    fig = go.Figure(layout_yaxis_range=[50, 200],
-                    layout_xaxis_range=[1950, 2099],
-                    layout_showlegend=False)
+    fig = go.Figure(layout_yaxis_range=[50, 200], layout_xaxis_range=[1950, 2099], layout_showlegend=False)
     fig.add_trace(go.Scatter(x=dff['Year'], y=dff['Modeled Mean']))
-    fig.add_trace(
-        go.Scatter(x=dff['Year'], y=dff['Modeled Min'], fill='tonexty',
-                   fillcolor='rgba(255, 0, 0, 0.7)'))
-    fig.add_trace(
-        go.Scatter(x=dff['Year'], y=dff['Modeled Max'], fill='tonexty',
-                   fillcolor='rgba(0, 230, 64, 0.1)'))
-    fig.add_trace(
-        go.Scatter(x=dff['Year'], y=dff['Days with Minimum Temperature Below 32°F'],
-                   mode='markers'))
-    fig.update_layout(margin=dict(l=35, r=35, t=35, b=35),
-                      title="Annual Days with Minimum Temperature Below 32°F - Massachusetts")
+    fig.add_trace(go.Scatter(x=dff['Year'], y=dff['Modeled Min'], fill='tonexty',fillcolor='rgba(255, 0, 0, 0.7)'))
+    fig.add_trace(go.Scatter(x=dff['Year'], y=dff['Modeled Max'], fill='tonexty',fillcolor='rgba(0, 230, 64, 0.1)'))
+    fig.add_trace(go.Scatter(x=dff['Year'], y=dff['Days with Minimum Temperature Below 32°F'], mode='markers'))
+    fig.update_layout(margin=dict(l=35, r=35, t=35, b=35), title="Annual Days with Minimum Temperature Below 32°F - Massachusetts")
     fig.update_yaxes(title_text='Days')
     return fig
 
