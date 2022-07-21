0ebc790fcf1f6a4947a3d3acda5f341d3860c787
Charming Data <adamschroeder.m@gmail.com>
2022-07-21 19:27:44 -0400

    Update app.py

--- Dash_More_Advanced_Shit/Intro to Python multipage/App-A/app.py
+++ Dash_More_Advanced_Shit/Intro to Python multipage/App-A/app.py
@@ -6,9 +6,9 @@ app = dash.Dash(__name__, use_pages=True)
 app.layout = html.Div(
     [
         # main app framework
-        html.Div("Python Multipage App with Dash", style={'fontSize':50}),
+        html.Div("Python Multipage App with Dash", style={'fontSize':50, 'textAlign':'center'}),
         html.Div([
-            dcc.Link(page['name']+"-------", href=page['path'])
+            dcc.Link(page['name']+"  |  ", href=page['path'])
             for page in dash.page_registry.values()
         ]),
         html.Hr(),
