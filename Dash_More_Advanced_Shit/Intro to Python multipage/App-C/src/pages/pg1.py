f306c14dc61a634216e7386e883320ae57f3b766
Adam <adamschroeder.m@gmail.com>
2022-08-21 19:12:03 -0400

    Update pg1.py

--- Dash_More_Advanced_Shit/Intro to Python multipage/App-C/src/pages/pg1.py
+++ Dash_More_Advanced_Shit/Intro to Python multipage/App-C/src/pages/pg1.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 dash.register_page(__name__,
                    path='/',  # '/' is home page and it represents the url
                    name='Home',  # name of page, commonly used as name of link
-                   title='Index',  # title that appears on browswer's tab
+                   title='Index',  # title that appears on browser's tab
                    image='pg1.png',  # image in the assets folder
                    description='Histograms are the new bar charts.'
 )
