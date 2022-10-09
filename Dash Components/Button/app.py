10d518ebeabfc74887718e331b1504203caf7196
Adam <adamschroeder.m@gmail.com>
2022-10-09 16:52:13 -0400

    Update app.py

--- Dash Components/Button/app.py
+++ Dash Components/Button/app.py
@@ -165,3 +165,43 @@ if __name__ == "__main__":
 #
 # if __name__ == "__main__":
 #     app.run_server(debug=True, port=8007)
+
+
+# -------------------------------------------------------------------------
+################# Mantine Button Connected to a Form ######################
+
+# from dash import Dash, html, Input, Output, State, ctx
+# import dash_bootstrap_components as dbc
+#
+# app = Dash(__name__, external_stylesheets=[dbc.themes.PULSE])
+#
+# app.layout = dbc.Container([
+#     dbc.FormFloating(
+#         [
+#             dbc.Input(id="username"),
+#             dbc.Label("Enter username"),
+#         ]
+#     ),
+#     dbc.Button(id="button1", children="Register Name"),
+#     dbc.Button(id="button2", children="Submit Application", className="ms-2"),
+#     html.Div(id="output_container", className="mt-4")
+# ], fluid=True)
+#
+#
+# @app.callback(
+#     Output("output_container", "children"),
+#     Input("button1", "n_clicks"),
+#     Input("button2", "n_clicks"),
+#     State("username", "value"),
+#     prevent_initial_call=True,
+# )
+# def greet(_, __, name):
+#     button_clicked = ctx.triggered_id
+#     if button_clicked == 'button1':
+#         return f"Welcome {name}!" if name else "Please enter username"
+#     elif button_clicked == 'button2':
+#         return "Your application has been submitted." if name else "Please enter username"
+#
+#
+# if __name__ == "__main__":
+#     app.run_server(debug=True, port=8008)
