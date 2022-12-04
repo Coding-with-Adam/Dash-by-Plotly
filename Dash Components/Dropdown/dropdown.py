3f9020030803a5ab4a81d6bf5a33d81339f2e2df
Adam <adam@charming-data.com>
2022-12-04 11:14:57 -0500

    Update Dash Components/Dropdown/dropdown.py

--- Dash Components/Dropdown/dropdown.py
+++ Dash Components/Dropdown/dropdown.py
@@ -1,11 +1,8 @@
 # Video:    [Dropdown Selector- Python Dash Plotly](https://youtu.be/UYH_dNSX1DM)
 # Docs:     [dcc.Dropdown](https://dash.plotly.com/dash-core-components/dropdown)
 #
-
 import pandas as pd     #(version 1.0.0)
-import plotly           #(version 4.5.4) pip install plotly==4.5.4
 import plotly.express as px
-
 import dash             #(version 1.9.1) pip install dash==1.9.1
 from dash import dcc 
 from dash import html
