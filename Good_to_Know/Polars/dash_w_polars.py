8c546394fd83a2a4ab071ff838d4ce3a4456cf76
Adam <adamschroeder.m@gmail.com>
2023-02-04 18:16:12 -0500

    Update dash_w_polars.py

--- Good_to_Know/Polars/dash_w_polars.py
+++ Good_to_Know/Polars/dash_w_polars.py
@@ -15,46 +15,50 @@ from datetime import datetime, date
 # READ DATA: FS_sp500_Value.csv--------------------------------------------
 # data source - https://www.kaggle.com/datasets/hanseopark/sp-500-stocks-value-with-financial-statement?resource=download
 
-# start = time.time()  # Pandas
-# df_pandas = pd.read_csv('FS_sp500_Value.csv')
-# end = time.time()
-# pd_read = round(end - start, 3)
-
-# start = time.time()  # Polars
-# df_polars = pl.read_csv('FS_sp500_Value.csv')
-# end = time.time()
-# pl_read = round(end - start,3)
-# compare_read_data = (pd_read / pl_read) * 100
-
-
-start = time.time()
-q1 = (
-    pl.scan_csv("FS_sp500_Value.csv")
-    .lazy()
-    .select(pl.col('Ticker')).unique()
-    .collect()
-)
-end = time.time()
-pl_read = round(end - start,3)
-print(pl_read)
-# print(q1.to_series().to_arrow())
-
 start = time.time()  # Pandas
 df_pandas = pd.read_csv('FS_sp500_Value.csv')
-ticker_values = df_pandas['Ticker'].unique()
 end = time.time()
 pd_read = round(end - start, 3)
+
+start = time.time()  # Polars
+df_polars = pl.read_csv('FS_sp500_Value.csv')
+end = time.time()
+pl_read = round(end - start,3)
 compare_read_data = (pd_read / pl_read) * 100
-print(f"polars is {round(compare_read_data)}% faster than pandas")
-exit()
+
+
+# # Make polars even faster with lazyload
+# # Polars - get unique ticker values to create dropdown options
+# start = time.time()
+# q1 = (
+#     pl.scan_csv("FS_sp500_Value.csv")
+#     .lazy()
+#     .select(pl.col('Ticker')).unique()
+# )
+# dropdown_pl = dcc.Dropdown(q1.collect().to_series().to_arrow(), value='ACN')
+# end = time.time()
+# pl_read = round(end - start,3)
+# print(pl_read)
+#
+# # Pandas - get unique ticker values to create dropdown options
+# start = time.time()
+# df_pandas = pd.read_csv('FS_sp500_Value.csv')
+# ticker_values = df_pandas['Ticker'].unique()
+# dropdown_pd = dcc.Dropdown(ticker_values, value='ACN')
+# end = time.time()
+# pd_read = round(end - start, 3)
+# print(pd_read)
+# compare_read_data = (pd_read / pl_read) * 100
+# print(f"polars is {round(compare_read_data)}% faster than pandas")
+# exit()
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 app.layout = dbc.Container([
     html.H1('Compare Dash App with Pandas vs Polars', style={'textAlign': 'center'}),
-    # html.Div(f"Reading data with polars: {pl_read}"),
-    # html.Div(f"Reading data with pandas: {pd_read}"),
-    # html.Div(f"polars is {round(compare_read_data)}% faster than pandas", className='mb-5'),
+    html.Div(f"Reading data with polars: {pl_read}"),
+    html.Div(f"Reading data with pandas: {pd_read}"),
+    html.Div(f"polars is {round(compare_read_data)}% faster than pandas", className='mb-5'),
 
     dbc.Row([
         dbc.Col([
@@ -109,83 +113,85 @@ app.layout = dbc.Container([
 ])
 
 
-# @callback(
-#     Output('datechart_pd', 'figure'),
-#     Output('datechart_pl', 'figure'),
-#     Output('polars_dates', 'children'),
-#     Output('pandas_dates', 'children'),
-#     Output('dates_compare', 'children'),
-#     Input('date_chosen', 'start_date'),
-#     Input('date_chosen', 'end_date')
-# )
-# def line_chart_date(start_d, end_d):
-#     date_start = datetime.strptime(start_d, '%Y-%m-%d')
-#     date_end = datetime.strptime(end_d, '%Y-%m-%d')
-#
-#     # PANDAS date filtering
-#     start = time.time()
-#     df_pandas['Date'] = pd.to_datetime(df_pandas['Date'])
-#     mask = (df_pandas['Date'] > date_start) & (df_pandas['Date'] <= date_end)
-#     pd_filtered = df_pandas.loc[mask]
-#     pd_filtered = pd_filtered[pd_filtered['Ticker']=='ACN']
-#     end = time.time()
-#     data_sliced_pd = end - start
-#
-#     fig_pd = px.line(pd_filtered, x='Date', y='High')
-#
-#     # POLARS date filtering
-#     start = time.time()
-#     pl_filtered = df_polars.filter(
-#         pl.col("Date").is_between(date_start,date_end),
-#     )
-#     pl_filtered = pl_filtered.filter(pl.col('Ticker') == 'ACN')
-#     end = time.time()
-#     data_sliced_pl = end - start
-#
-#     fig_pl = px.line(x=pl_filtered.select('Date').to_series(), y=pl_filtered.select('High').to_series())
-#
-#
-#     comparison = (data_sliced_pd / data_sliced_pl) *100
-#     return fig_pd, \
-#            fig_pl, \
-#            f"Data sliced with polars: {round(data_sliced_pl, 3)}", \
-#            f"Data sliced with pandas: {round(data_sliced_pd, 3)}", \
-#            f"polars is {round(comparison)}% faster than pandas"
-#
-#
-# @callback(
-#     Output('chart_pd', 'figure'),
-#     Output('chart_pl', 'figure'),
-#     Output('polars_new_col', 'children'),
-#     Output('pandas_new_col', 'children'),
-#     Output('new_col_compare', 'children'),
-#     Input('my_dpdn', 'value')
-# )
-# def line_chart_date(value):
-#     # PANDAS create new column
-#     start = time.time()
-#     df_pandas['price'] = df_pandas[value] * df_pandas['Volume']
-#     end = time.time()
-#     data_sliced_pd = end - start
-#
-#     pd_filtered = df_pandas[df_pandas['Ticker'] == 'ACN']
-#     fig_pd = px.histogram(pd_filtered, x='price')
-#
-#     # POLARS create new column
-#     start = time.time()
-#     df_pl = df_polars.with_columns([(pl.col(value) * pl.col('Volume')).alias("price")])
-#     end = time.time()
-#     data_sliced_pl = end - start
-#
-#     pl_filtered = df_pl.filter(pl.col('Ticker') == 'ACN')
-#     fig_pl = px.histogram(x=pl_filtered.select('price').to_series())
-#
-#     comparison = (data_sliced_pd / data_sliced_pl) *100
-#     return fig_pd, \
-#            fig_pl, \
-#            f"Data sliced with polars: {round(data_sliced_pl, 3)}", \
-#            f"Data sliced with pandas: {round(data_sliced_pd, 3)}", \
-#            f"polars is {round(comparison)}% faster than pandas"
+@callback(
+    Output('datechart_pd', 'figure'),
+    Output('datechart_pl', 'figure'),
+    Output('polars_dates', 'children'),
+    Output('pandas_dates', 'children'),
+    Output('dates_compare', 'children'),
+    Input('date_chosen', 'start_date'),
+    Input('date_chosen', 'end_date')
+)
+def line_chart_date(start_d, end_d):
+    date_start = datetime.strptime(start_d, '%Y-%m-%d')
+    date_end = datetime.strptime(end_d, '%Y-%m-%d')
+
+    # PANDAS date filtering
+    start = time.time()
+    df_pandas['Date'] = pd.to_datetime(df_pandas['Date'])
+    mask = (df_pandas['Date'] > date_start) & (df_pandas['Date'] <= date_end)
+    pd_filtered = df_pandas.loc[mask]
+    pd_filtered = pd_filtered[pd_filtered['Ticker']=='ACN']
+    end = time.time()
+    data_sliced_pd = end - start
+
+    fig_pd = px.line(pd_filtered, x='Date', y='High')
+
+    # POLARS date filtering
+    start = time.time()
+    pl_filtered = df_polars.filter(
+        pl.col("Date").is_between(date_start,date_end),
+    )
+    pl_filtered = pl_filtered.filter(pl.col('Ticker') == 'ACN')
+    end = time.time()
+    data_sliced_pl = end - start
+
+    fig_pl = px.line(x=pl_filtered.select('Date').to_series(), y=pl_filtered.select('High').to_series())
+
+
+    comparison = (data_sliced_pd / data_sliced_pl) *100
+    return fig_pd, \
+           fig_pl, \
+           f"Data sliced with polars: {round(data_sliced_pl, 3)}", \
+           f"Data sliced with pandas: {round(data_sliced_pd, 3)}", \
+           f"polars is {round(comparison)}% faster than pandas"
+
+
+@callback(
+    Output('chart_pd', 'figure'),
+    Output('chart_pl', 'figure'),
+    Output('polars_new_col', 'children'),
+    Output('pandas_new_col', 'children'),
+    Output('new_col_compare', 'children'),
+    Input('my_dpdn', 'value')
+)
+def line_chart_date(value):
+    # PANDAS create new column
+    start = time.time()
+    df_pandas['price'] = df_pandas[value] * df_pandas['Volume']
+    end = time.time()
+    data_sliced_pd = end - start
+
+    pd_filtered = df_pandas[df_pandas['Ticker'] == 'ACN']
+    fig_pd = px.histogram(pd_filtered, x='price')
+
+    # POLARS create new column
+    start = time.time()
+    df_pl = df_polars.with_columns([(pl.col(value) * pl.col('Volume')).alias("price")])
+    end = time.time()
+    data_sliced_pl = end - start
+
+    pl_filtered = df_pl.filter(pl.col('Ticker') == 'ACN')
+    fig_pl = px.histogram(x=pl_filtered.select('price').to_series())
+
+    print(data_sliced_pd)
+    print(data_sliced_pl)
+    comparison = (data_sliced_pd / data_sliced_pl) *100
+    return fig_pd, \
+           fig_pl, \
+           f"Data sliced with polars: {round(data_sliced_pl, 3)}", \
+           f"Data sliced with pandas: {round(data_sliced_pd, 3)}", \
+           f"polars is {round(comparison)}% faster than pandas"
 
 
 if __name__=='__main__':
