a2f75fe1d0e4086c0cadf00437afabd3d3c01054
Adam <adamschroeder.m@gmail.com>
2023-02-19 15:07:23 -0500

    Update example.py

--- Dash_More_Advanced_Shit/Email_Alerts/example.py
+++ Dash_More_Advanced_Shit/Email_Alerts/example.py
@@ -4,16 +4,19 @@ from email.message import EmailMessage  # for sending emails
 
 from dash import Dash, dcc, html, Output, Input, State      # pip install dash
 import plotly.express as px
+import pandas as pd                                         # pip install pandas
 from datetime import datetime
 
+ticker_list = pd.read_csv('https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Dash_More_Advanced_Shit/Email_Alerts/technology_stocks.csv')
+
 # Thank you to YouTube user @ClarityCoders for this function.
-def email_alert(subject, body, to):
+def send_alert(subject, body, to):
     msg = EmailMessage()
     msg.set_content(body)
     msg['subject'] = subject
     msg['to'] = to
 
-    user = "tutorialemailonly@gmail.com"
+    user = 'yourOwnEmail@gmail.com' 
     msg['from'] = user
     password = 'AppPasswordFromGoogSecurityTab'
 
@@ -27,14 +30,14 @@ def email_alert(subject, body, to):
 
 app = Dash(__name__)
 app.layout = html.Div([
-    html.H1("Stock Market Alert System", style={'textAlign': 'center'}),
+    html.H1("Stock Market Alert System (Email and SMS)", style={'textAlign': 'center'}),
     dcc.Interval(id='trigger', interval=1000*10), # 10 seconds
-    dcc.Dropdown(id='ticker-name', options=['TSLA','FB','AMZN'], value='TSLA', clearable=False, style={'width': '25%'}),
+    dcc.Dropdown(id='ticker-name', options=ticker_list['Ticker'], value='TSLA', clearable=False, style={'width': '50%'}),
     html.Div(id='price-placeholder', children=[]),
     dcc.Graph(id='line-history', figure={}),
     html.Hr(),
 
-    html.Div('Whould you like to set up email or phone alerts for price changes?'),
+    html.Div('Would you like to set up email or phone alerts for price changes?'),
     dcc.RadioItems(id='alert-permission', options=['No','Yes, email alerts', 'Yes, phone alerts'], value='No'),
     html.Div('Alert me when share price is equal or above:'),
     dcc.Input(id='alert-value', type='number', min=0, max=1000, value=0),
@@ -50,21 +53,21 @@ app.layout = html.Div([
     State('alert-value', 'value'),
 )
 def display_price(_, ticker_name, alert_permission, alert_value):
-    fmp = FMP(output_format='pandas', api_key='f9cd560adc58cf98f76431a5da5e06b8')
+    fmp = FMP(output_format='pandas', api_key='YourFMPdeveloperAPIkey')
     stock = fmp.get_quote_short(ticker_name)
     stock_history = fmp.get_historical_chart('1hour', ticker_name)
     current_time = datetime.now().strftime("%H:%M:%S")
 
     if alert_permission == 'Yes, phone alerts':
         if stock.price[0] >= alert_value:
-            email_alert('Alert: Buy Stock',
+            send_alert('Alert: Buy Stock',
                         f'{ticker_name} passed your alert threshold of ${alert_value} '
                         f'and is now at ${stock.price[0]} per share.',
                         '3475140963@vtext.com')
 
     elif alert_permission == 'Yes, email alerts':
         if stock.price[0] >= alert_value:
-            email_alert('Alert: Buy Stock',
+            send_alert('Alert: Buy Stock',
                         f'{ticker_name} passed your alert threshold of ${alert_value} '
                         f'and is now at ${stock.price[0]} per share.',
                         'tutorialemailonly@gmail.com')
