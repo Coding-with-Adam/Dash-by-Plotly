d22c043b46529fd6d1b3ad9a8fd9dad7cb79a93f
Orcé MARINKOVSKI <Orc MARINKOVSKI>
2023-07-23 07:44:31 +0200

    fix for label in dataset

--- Bootstrap/Complete_Guide/live_bootstrap.py
+++ Bootstrap/Complete_Guide/live_bootstrap.py
@@ -16,6 +16,7 @@ df = web.DataReader(['AMZN','GOOGL','FB','PFE','MRNA','BNTX'],
                     'stooq', start=start, end=end)
 # df=df.melt(ignore_index=False, value_name="price").reset_index()
 df = df.stack().reset_index()
+df.rename(columns={'level_0': 'Date'}, inplace=True)
 print(df[:15])
 
 # df.to_csv("mystocks.csv", index=False)
