423f6449d198924fc4f72213b57eadbfba8e8819
Adam <adam@charming-data.com>
2024-01-21 10:04:39 -0500

    Update app.py

--- AI/EmbedChain-Chatbot/app.py
+++ AI/EmbedChain-Chatbot/app.py
@@ -4,18 +4,22 @@ from embedchain import App
 # pip install -r requirements.txt
 
 # Create a bot instance
-os.environ["OPENAI_API_KEY"] = "my-access-token-here"
+os.environ["OPENAI_API_KEY"] = "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"
 ai_bot = App.from_config(config_path="config.yaml")
 
 # Embed resources: websites, PDFs, videos
-ai_bot.add("https://nycaudubon.org/our-work/conservation/project-safe-flight")
-# ai_bot.add("https://www.wildbirdfund.org/page-sitemap.xml", data_type="sitemap")
+ai_bot.add("https://www.wildbirdfund.org/page-sitemap.xml", data_type="sitemap")
+# ai_bot.add("https://nycaudubon.org/our-work/conservation/project-safe-flight")
 # ai_bot.add("Birds Flying Into Windows.pdf", data_type='pdf_file')
 # ai_bot.add("https://www.youtube.com/watch?v=l8LDTRxc0Bc")
 
 app = Dash()
 app.layout = html.Div([
-    html.H1('Learn to Keep Birds safe'),
+    html.H1('WILD BIRD FUND'),
+    html.H3('This AI Chatbot was trained on the wildbirdfund website. It was built to help people understand how to keep city birds '
+            'safe and take care of them when injured.'),
+    html.Label('Ask your question:'),
+    html.Br(),
     dcc.Textarea(id='question-area', value=None, style={'width': '25%', 'height': 100}),
     html.Br(),
     html.Button(id='submit-btn', children='Submit'),
