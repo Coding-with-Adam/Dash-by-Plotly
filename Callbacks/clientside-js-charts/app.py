fccc8afd6cc830b9250aed55ea27774b23d63de6
Adam <adam@charming-data.com>
2024-01-28 16:37:59 -0500

    Create app.py

---
+++ Callbacks/clientside-js-charts/app.py
@@ -0,0 +1,45 @@
+from dash import  Dash, dcc, html, Input, Output, clientside_callback, ClientsideFunction, register_page
+import dash_mantine_components as dmc
+from data import tradeData
+
+
+app = Dash(__name__, external_scripts=['https://cdn.jsdelivr.net/npm/apexcharts'])
+app.layout = html.Div(
+    children=[
+        dcc.Store(id='ApexchartsSampleData', data=tradeData),
+        html.H1("Javascript Charts inside a Dash App"),
+        dmc.Center(
+            dmc.Paper(
+                shadow="sm",
+                style={'height':'600px', 'width':'800px', 'marginTop':'100px'},
+                children=[
+                    html.Div(id='apexAreaChart'),
+                    dmc.Center(
+                        children=[
+                            dmc.SegmentedControl(
+                                id="selectCountryChip",
+                                value="Canada",
+                                data=['Canada', 'USA', 'Australia'],
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+    ]
+)
+
+
+clientside_callback(
+    ClientsideFunction(
+        namespace='apexCharts',
+        function_name='areaChart'
+    ),
+    Output("apexAreaChart", "children"),
+    Input("ApexchartsSampleData", "data"),
+    Input("selectCountryChip", "value"),
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
