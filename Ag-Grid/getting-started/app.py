198af0069537e5f8f1649bd2832b6f2a98a2cfa2
Adam <adam@charming-data.com>
2024-03-03 15:30:05 -0500

    Update app.py

--- Ag-Grid/getting-started/app.py
+++ Ag-Grid/getting-started/app.py
@@ -1,19 +1,44 @@
-import dash_ag_grid as dag
-from dash import Dash
+import micropip
+await micropip.install("dash_ag_grid")
+await micropip.install("dash-mantine-components")
+await micropip.install("urllib3")
+await micropip.install('openpyxl')
+
+
 import pandas as pd
+import plotly.express as px
+from dash import Dash, html, dcc
+import dash_ag_grid as dag
+import dash_mantine_components as dmc
+import base64
+def create_onedrive_directdownload (onedrive_link):
+    data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
+    data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+    resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+    print(resultUrl)
+    return resultUrl
 
-# df = pd.read_csv("space-mission-data.csv") # if your excel sheet is located in your computer
-
-# Incorporat data from excel sheet on Google Drive
-url = 'https://drive.google.com/file/d/1OPxHt2VimBAa4YXoLpSL7lHIJdR45XbU/view?usp=sharing' # replace this with your own link
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df = pd.read_csv(path)
+url = create_onedrive_directdownload("https://1drv.ms/x/s!AoKPiuofdvp_ltlppz8OsI8UmSVP1w?e=UVI006")
+print(url)
+df = pd.read_excel(url, engine='openpyxl')
+print(df)
 
 app = Dash(__name__)
-app.layout = dag.AgGrid(
-    rowData=df.to_dict("records"),
-    columnDefs=[{"field": i} for i in df.columns],
-)
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+app.layout = html.Div([
+    dmc.Alert(
+    "Hi from Dash Mantine Components. You can create some great looking dashboards using me!",
+    title="Welcome!",
+    color="violet"),
+    dag.AgGrid(
+        id="grid",
+        rowData=df.to_dict("records"),
+        columnDefs=[{"field": i} for i in df.columns],
+        defaultColDef={"filter": True},
+        columnSize="sizeToFit",
+    ),
+    dcc.Graph(figure=px.histogram(df, x=df.columns[1]))
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
